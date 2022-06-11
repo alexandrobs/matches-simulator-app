84bd6269b3992df4f5aebb9df2e1f84ef75deae5
alexandrobs <jornalsinal@gmail.com>
2022-06-11 00:44:18 -0300

    add botao swipe e lista de partidas

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -1,11 +1,22 @@
 package me.dio.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //exemplo de findViewById sem viewBinding
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        //exemplo de findViewById com viewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding.tvHello.text = "Teste"
+        setContentView(binding.root)
     }
 }
